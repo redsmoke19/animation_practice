1befbf9a907ef810918baabe7296e0c5a4582599
Oleg Tsyganov <TsyganovDev@gmail.com>
2021-02-01 20:34:27 +0400

    Bug fix

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,13 @@
 // modules
-import mobileHeight from './modules/mobile-height-adjust.js';
-import slider from './modules/slider.js';
-import menu from './modules/menu.js';
-import footer from './modules/footer.js';
-import chat from './modules/chat.js';
-import result from './modules/result.js';
-import form from './modules/form.js';
-import social from './modules/social.js';
-import FullPageScroll from './modules/full-page-scroll';
+import mobileHeight from "./modules/mobile-height-adjust.js";
+import slider from "./modules/slider.js";
+import menu from "./modules/menu.js";
+import footer from "./modules/footer.js";
+import chat from "./modules/chat.js";
+import result from "./modules/result.js";
+import form from "./modules/form.js";
+import social from "./modules/social.js";
+import FullPageScroll from "./modules/full-page-scroll";
 
 // init modules
 mobileHeight();
@@ -21,3 +21,15 @@ social();
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
+
+window.addEventListener(`load`, () => {
+  let bbb = document.querySelector(`body`);
+  bbb.classList.add(`ready`);
+  let introSection = document.querySelector(`.screen--intro`);
+  let introMessage = document.querySelector(`.intro__message`);
+  if (introSection.classList.contains(`active`)) {
+    introMessage.classList.add(`active`);
+  } else {
+    introMessage.classList.remove(`active`);
+  }
+});
